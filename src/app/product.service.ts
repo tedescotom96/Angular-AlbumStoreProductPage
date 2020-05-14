5a69ad9e54a44dbde78b8b87e75803b74ce89fb0
Thomas Tedesco <ttedesc@bgsu.eud>
2020-05-14 15:33:19 -0400

    updated 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,12 +6,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _albumURL = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http:Http) {  }
 
   getAlbum(id:number){
-    return this._http.get(this._albumURL).map((Response)=>Response.json());
+    return this._http.get(this._albumUrl).map((response)=>response.json());
   }
 
 
